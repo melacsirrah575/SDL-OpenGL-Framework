a6ce52956cf73ba30fa9eef04d40ea0193c84969
Calem Harris <melacsirrah575@gmail.com>
2024-05-09 21:31:47 -0300

    Adding PlatformManager to SDLFramework namespace.

--- PlatformExperimenting/PlatformManager.cpp
+++ PlatformExperimenting/PlatformManager.cpp
@@ -1,83 +1,85 @@
 #include "PlatformManager.h"
 
-PlatformManager* PlatformManager::sInstance = nullptr;
+namespace SDLFramework {
+	PlatformManager* PlatformManager::sInstance = nullptr;
 
-PlatformManager* PlatformManager::Instance() {
-	if (sInstance == nullptr) {
-		sInstance = new PlatformManager();
+	PlatformManager* PlatformManager::Instance() {
+		if (sInstance == nullptr) {
+			sInstance = new PlatformManager();
+		}
+
+		return sInstance;
 	}
 
-	return sInstance;
-}
-
-void PlatformManager::Release() {
-	delete sInstance;
-	sInstance = nullptr;
-}
-
-void PlatformManager::AddPlatformToMap(Platform* platformToAdd) {
-	if (mPlatforms.find(platformToAdd->GetId()) != mPlatforms.end()) {
-		//We have already added this platform!
-		std::cout << "We have already added Platform " << platformToAdd->GetId() << ". Did you mean to GetPlatform?" << std::endl;
-		return;
+	void PlatformManager::Release() {
+		delete sInstance;
+		sInstance = nullptr;
 	}
 
-	mPlatforms[platformToAdd->GetId()] = platformToAdd;
-	mMapHasPlatforms = true;
-}
+	void PlatformManager::AddPlatformToMap(Platform* platformToAdd) {
+		if (mPlatforms.find(platformToAdd->GetId()) != mPlatforms.end()) {
+			//We have already added this platform!
+			std::cout << "We have already added Platform " << platformToAdd->GetId() << ". Did you mean to GetPlatform?" << std::endl;
+			return;
+		}
 
-Platform* PlatformManager::GetPlatform(unsigned long ID) {
-	if (mPlatforms.find(ID) != mPlatforms.end()) {
-		return mPlatforms[ID];
+		mPlatforms[platformToAdd->GetId()] = platformToAdd;
+		mMapHasPlatforms = true;
 	}
-	else {
-		std::cerr << "Trying to get a platform that doesn't exist! Returning nullptr" << std::endl;
-		return nullptr;
-	}
-}
 
-Vector2 PlatformManager::GetPlatformPosition(unsigned long ID) {
-	if (mPlatforms.find(ID) != mPlatforms.end()) {
-		return mPlatforms[ID]->Position();
+	Platform* PlatformManager::GetPlatform(unsigned long ID) {
+		if (mPlatforms.find(ID) != mPlatforms.end()) {
+			return mPlatforms[ID];
+		}
+		else {
+			std::cerr << "Trying to get a platform that doesn't exist! Returning nullptr" << std::endl;
+			return nullptr;
+		}
 	}
-	else {
-		std::cerr << "Trying to get a platform that doesn't exist! Returning 0,0" << std::endl;
-		return Vec2_Zero;
+
+	Vector2 PlatformManager::GetPlatformPosition(unsigned long ID) {
+		if (mPlatforms.find(ID) != mPlatforms.end()) {
+			return mPlatforms[ID]->Position();
+		}
+		else {
+			std::cerr << "Trying to get a platform that doesn't exist! Returning 0,0" << std::endl;
+			return Vec2_Zero;
+		}
 	}
-}
 
-void PlatformManager::CreateNewPlatform(bool standable, Vector2 position, GLTexture* texture, Vector2 scale , Vector2 boxColliderOffset) {
-	Platform* platform = new Platform(standable, position, texture, scale, boxColliderOffset);
-	AddPlatformToMap(platform);
-}
-
-PlatformManager::~PlatformManager() {
-	for (auto it = mPlatforms.begin(); it != mPlatforms.end(); it++) {
-		delete it->second;
+	void PlatformManager::CreateNewPlatform(bool standable, Vector2 position, GLTexture* texture, Vector2 scale, Vector2 boxColliderOffset) {
+		Platform* platform = new Platform(standable, position, texture, scale, boxColliderOffset);
+		AddPlatformToMap(platform);
 	}
-}
 
-void PlatformManager::Update() {
-	if (mMapHasPlatforms) {
+	PlatformManager::~PlatformManager() {
 		for (auto it = mPlatforms.begin(); it != mPlatforms.end(); it++) {
-			if (it->second) {
-				it->second->Update();
-			}
-			else {
-				std::cerr << "Error: Null pointer in map for key " << it->first << std::endl;
+			delete it->second;
+		}
+	}
+
+	void PlatformManager::Update() {
+		if (mMapHasPlatforms) {
+			for (auto it = mPlatforms.begin(); it != mPlatforms.end(); it++) {
+				if (it->second) {
+					it->second->Update();
+				}
+				else {
+					std::cerr << "Error: Null pointer in map for key " << it->first << std::endl;
+				}
 			}
 		}
 	}
-}
 
-void PlatformManager::Render() {
-	if (mMapHasPlatforms) {
-		for (auto it = mPlatforms.begin(); it != mPlatforms.end(); ++it) {
-			if (it->second) {
-				it->second->Render();
-			}
-			else {
-				std::cerr << "Error: Null pointer in map for key " << it->first << std::endl;
+	void PlatformManager::Render() {
+		if (mMapHasPlatforms) {
+			for (auto it = mPlatforms.begin(); it != mPlatforms.end(); ++it) {
+				if (it->second) {
+					it->second->Render();
+				}
+				else {
+					std::cerr << "Error: Null pointer in map for key " << it->first << std::endl;
+				}
 			}
 		}
 	}
--- PlatformExperimenting/PlatformManager.h
+++ PlatformExperimenting/PlatformManager.h
@@ -2,26 +2,28 @@
 #include "Platform.h"
 #include "map"
 
-class PlatformManager {
-private:
-	static PlatformManager* sInstance;
+namespace SDLFramework {
+	class PlatformManager {
+	private:
+		static PlatformManager* sInstance;
 
-	std::map<unsigned long, Platform*> mPlatforms;
-	bool mMapHasPlatforms = false;
+		std::map<unsigned long, Platform*> mPlatforms;
+		bool mMapHasPlatforms = false;
 
-	PlatformManager() { };
-	~PlatformManager();
+		PlatformManager() { };
+		~PlatformManager();
 
-public:
-	static PlatformManager* Instance();
-	static void Release();
+	public:
+		static PlatformManager* Instance();
+		static void Release();
 
-	void CreateNewPlatform(bool Standable, Vector2 position, GLTexture* texture, Vector2 scale, Vector2 boxColliderOffset = Vec2_Zero);
-	void AddPlatformToMap(Platform* platformToAdd);
-	
-	Platform* GetPlatform(unsigned long ID);
-	Vector2 GetPlatformPosition(unsigned long ID);
+		void CreateNewPlatform(bool Standable, Vector2 position, GLTexture* texture, Vector2 scale, Vector2 boxColliderOffset = Vec2_Zero);
+		void AddPlatformToMap(Platform* platformToAdd);
 
-	void Update();
-	void Render();
-};
+		Platform* GetPlatform(unsigned long ID);
+		Vector2 GetPlatformPosition(unsigned long ID);
+
+		void Update();
+		void Render();
+	};
+}
